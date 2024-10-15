3488ed656422eeee030730e9c06ee072d72d8dfb
riktadhali <riktadhali031@gmail.com>
2024-10-15 14:09:30 +0600

    first commit

---
+++ length.js
@@ -0,0 +1,12 @@
+// const numbers =[2,4,3,45,76,85,447,94,];
+// console.log(numbers.length);
+// console.log(numbers[4]);
+// const x =numbers[4];
+// console.log(numbers.indexOF(447)); /// cheak index
+// numbers.push(9);
+// console.log( numbers);
+const team = ["rikta","jannat","mitu","nusrat",];
+// team.push("tasmim");
+// console.log(team);
+team.unshift("tasmim");
+console.log(team);
---
+++ script.js
@@ -0,0 +1,8 @@
+// var x=20;
+// var y=40;
+// console.log(x+y);
+
+
+
+const x =[1,2,3,34,54,46,44];
+console.log(x);
